fddf9028fe29879938be402377b5894da104c341
Tiago Vinícius de Oliveira Brachini <tvobrachini@gmail.com>
2015-04-27 22:05:49 -0300

    sending template files

--- app2/artigos/admin.py
+++ app2/artigos/admin.py
@@ -18,7 +18,12 @@ class ArtigoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'agencia',  'url', 'pub_date')
     search_fields = ('titulo', 'agencia')
     list_filter = ('pub_date',)
+    date_hierarchy = 'pub_date'
+    ordering = ('-pub_date',)
+    fields = ('titulo', 'url', 'pub_date', 'autores', 'agencia', 'conteudo')
+    prepopulated_fields = {'url': ('titulo',)}
+
 
 admin.site.register(Agencia, AgenciaAdmin)
 admin.site.register(Autor, AutorAdmin)
-admin.site.register(Artigo)
+admin.site.register(Artigo, ArtigoAdmin)
