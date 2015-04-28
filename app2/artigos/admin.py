afc9162bbfaa50845c65df197869bffc3a61a0cd
Tiago Vinícius de Oliveira Brachini <tvobrachini@gmail.com>
2015-04-27 21:45:26 -0300

    Adding model admin

--- app2/artigos/admin.py
+++ app2/artigos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Agencia
+from .models import Autor
+from .models import Artigo
 
-# Register your models here.
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+
+
+class AgenciaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'site')
+    search_fields = ('nome', 'site')
+
+
+class ArtigoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'agencia',  'url', 'pub_date')
+    search_fields = ('titulo', 'agencia')
+    list_filter = ('pub_date',)
+
+admin.site.register(Agencia, AgenciaAdmin)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Artigo)
--- app2/artigos/models.py
+++ app2/artigos/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from django.db.models import permalink
+# from django.db.models import permalink
 
 
 # Create your models here.
 class Agencia(models.Model):
     nome = models.CharField(max_length=50)
     site = models.URLField()
+
+    class Meta:
+        verbose_name = 'Agência'
 
     def __str__(self):
         return self.nome
@@ -15,20 +18,23 @@ class Autor(models.Model):
     nome = models.CharField(max_length=50)
     email = models.EmailField()
 
-    def _str__(self):
+    class Meta:
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
         return self.nome
 
 
 class Artigo(models.Model):
-    titulo = models.CharField('Título: ', max_length=80)
+    titulo = models.CharField('Título ', max_length=80)
     url = models.SlugField(
         'URL',
         max_length=200,
         help_text='URL baseada no título.',
         unique=True
     )
-    pub_date = models.DateTimeField('Data de Publicação: ')
-    conteudo = models.TextField('Conteúdo da Página: ')
+    pub_date = models.DateTimeField('Data de Publicação ')
+    conteudo = models.TextField('Conteúdo da Página ')
     autores = models.ManyToManyField(Autor)
     agencia = models.ForeignKey(Agencia)
 
